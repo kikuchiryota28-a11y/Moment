039f180e153c6b1f508cdf290ec9508a21bd2697
R 9999 <kikuchiryota28@gmail.com>
2026-09-17 21:54:29 +0900

    refactor: scope 3D canvas to interactive home scene

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import { BottomNavigation } from "@/components/layout/BottomNavigation";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
-import { Ambient3DCanvas } from "@/components/v3/Ambient3DCanvas";
 import type { ThemePreference } from "@/types/database";
 
 export default async function MainLayout({ children }: { children: React.ReactNode }) {
@@ -14,9 +13,8 @@ export default async function MainLayout({ children }: { children: React.ReactNo
 
   return (
     <ThemeProvider initialTheme={theme}>
-      <Ambient3DCanvas />
       <div className="relative z-10 min-h-screen">
-        <main className="mx-auto min-h-screen w-full max-w-3xl px-4 pb-28 sm:px-6">
+        <main className="mx-auto min-h-screen w-full px-4 pb-28 sm:px-6">
           {children}
         </main>
         <BottomNavigation />
